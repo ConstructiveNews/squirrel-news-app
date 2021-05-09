c480d0ad9217c75c8317d12facfaed090a4a3957
Oliver Erxleben <oliver.erxleben@me.com>
2021-05-09 18:24:03 +0200

    added Device default language

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { BaseLayout } from './layouts/BaseLayout';
 import { LANGUAGES } from './models';
 import { addFav, removeFav} from './api/favorites';
 
-  
+import { Plugins } from '@capacitor/core';
+
+const { Device } = Plugins;
+
 export const App: FC<{}> = () => {
 
   // const [articles, setArticles] =  useState <any>();
@@ -12,16 +15,12 @@ export const App: FC<{}> = () => {
   // getArticle("HVG0cAArZe6bMj4QJPag", "bvMDfEYH7xZ3iRv5YsXr", (result) => {
   //   console.log('fav', result);
   // });
-
-  getIssue(1, LANGUAGES.DE, (data) => {
-    // console.log('get issue', data.issueId)
-    
-    addFav(data.issueId, data.articles[0].articleId, () => { console.log('added')});
-    removeFav(data.issueId, data.articles[0].articleId, () => { console.log('removed')})
-  })
   
   // getFavorites().then( (data) => console.log('favs', data))
   
+  console.log('lang', Device.getLanguageCode())
+  
+
   return (
     <div>
       {/* <FAB></FAB> */}
