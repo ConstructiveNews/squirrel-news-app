251ef691eea7f885b70ea1055973ed85807476aa
Oliver Erxleben <oliver.erxleben@me.com>
2021-05-09 18:23:43 +0200

    added language dict

---
+++ src/models/language.ts
@@ -0,0 +1,113 @@
+export const DE = {
+  article: {
+    more: 'Mehr...',
+    photo: 'Foto'
+  },
+  issues: {
+    all: 'Alle Ausgaben',
+    previous: 'Vorherige Ausgabe',
+    favorites: 'Favoriten',
+    noMoreIssue: 'Keine weiteren Ausgaben vorhanden.'
+  },
+  lang: {
+    switch: 'Englische Version'
+  },
+  links: {
+    instagram: 'https://www.instagram.com/squirrelnews.de/',
+    twitter: 'https://twitter.com/squirrelnews_de',
+    facebook: 'https://www.facebook.com/squirrelnews.de'
+  },
+  privacyPolicy: {
+    name: 'Datenschutz',
+    url: 'https://squirrel-news.net/de/datenschutz/',
+    path: 'privacy-de'
+  },
+  imprint: {
+    name: 'Impressum',
+    url: 'https://squirrel-news.net/de/impressum/',
+    path: 'imprint-de'
+  },
+  contact: {
+    name: 'Kontakt',
+    url: 'https://squirrel-news.net/de/kontakt/'
+  },
+  support: {
+    name: 'Unterstützen',
+    url: 'https://squirrel-news.net/de/unterstuetzen/'
+  },
+  about: {
+    name: 'Über uns',
+    url: 'https://squirrel-news.net/de/ueber-uns/',
+    path: 'about-de'
+  },
+  share: {
+    title: 'News teilen!',
+    text: 'Schau dir das mal an...',
+    noSharingOptionAvailable: 'Keine Funktion zum Teilen verfügbar'
+  },
+  app: {
+    loading: 'Lade...'
+  },
+  donate: {
+    ownCase: 'in eigener Sache',
+    title: 'Würdest Du uns unterstützen?',
+    text: 'Squirrel News finanziert sich ausschließlich aus kleinen und mittleren Spenden. Dafür brauchen wir auch dich! Bitte unterstütze uns mit einem monatlichen oder jährlichen Betrag in Höhe deiner Wahl!'
+  }
+};
+
+export const EN = {
+  article: {
+    more: 'More...',
+    photo: 'Photo'
+  },
+  issues: {
+    all: 'All Issues',
+    previous: 'Previous Issue',
+    favorites: 'Favourites',
+    noMoreIssue: 'No more issues available.'
+  },
+  lang: {
+    switch: 'German Version'
+  },
+  links: {
+    instagram: 'https://www.instagram.com/squirrelnews/',
+    twitter: 'https://twitter.com/squirrelnews_en',
+    facebook: 'https://www.facebook.com/squirrelnews'
+  },
+  privacyPolicy: {
+    name: 'Privacy Policy',
+    url: 'https://squirrel-news.net/privacy-policy/',
+    path: 'privacy-en'
+  },
+  imprint: {
+    name: 'Imprint',
+    url: 'https://squirrel-news.net/imprint/',
+    path: 'imprint-en'
+  },
+  contact: {
+    name: 'Contact',
+    url: 'https://squirrel-news.net/contact/'
+  },
+  support: {
+    name: 'Support Us',
+    url: 'https://squirrel-news.net/support-us/'
+  },
+  about: {
+    name: 'About Us',
+    url: 'https://squirrel-news.net/about-us/',
+    path: 'about-en'
+  },
+  share: {
+    title: 'share the news!',
+    text: 'Have a look...',
+    noSharingOptionAvailable: 'No functionality to share available'
+  },
+  app: {
+    loading: 'Loading...'
+  },
+  donate: {
+    ownCase: 'in own case',
+    title: 'Would you support us?',
+    text: 'Squirrel News finanziert sich ausschließlich aus kleinen und mittleren Spenden. Dafür brauchen wir auch dich! Bitte unterstütze uns mit einem monatlichen oder jährlichen Betrag in Höhe deiner Wahl!'
+  }
+};
